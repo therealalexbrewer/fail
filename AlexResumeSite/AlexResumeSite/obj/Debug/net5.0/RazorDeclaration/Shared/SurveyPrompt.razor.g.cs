d6824a37d625cae5634615efe252c4c0881bcf69
therealalexbrewer <therealalexbrewer@gmail.com>
2021-04-27 15:29:30 -0400

    More changes to route new page
    Changes to the config files to get Home to be the index page for the site.

--- AlexResumeSite/AlexResumeSite/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ AlexResumeSite/AlexResumeSite/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace AlexResumeSite.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 1 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 2 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 3 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 4 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 5 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 6 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 7 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 8 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 9 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using AlexResumeSite;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\_Imports.razor"
+#line 10 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\_Imports.razor"
 using AlexResumeSite.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using AlexResumeSite.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Lovin Al\Source\Repos\AlexResumeSite\AlexResumeSite\Shared\SurveyPrompt.razor"
+#line 12 "C:\Users\Lovin Al\Documents\GitHub\therealalexbrewer.github.io\AlexResumeSite\AlexResumeSite\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
